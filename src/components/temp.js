13f0a76f613fc6d069bf991436bef6cda0a75103
bethuel <bethuel.pg@gmail.com>
2021-12-02 15:51:34 -0300

    02/12 - 16h

---
+++ src/components/temp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Text, Button, View} from 'react-native';
+import Estilo from './estilo';
+
+export default props => {
+  return <Text style={Estilo.Font}></Text>;
+};
